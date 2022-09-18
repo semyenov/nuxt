c0af242f7ee3c6adb535128deb5deae90bbddd0d
Alexander Semyenov <semyenov@hotmail.com>
2022-09-18 23:49:39 +0300

    darkMode

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,6 +4,9 @@ definePageMeta({
   title: 'Page Title',
 })
 
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+
 const { x, y } = useMouse()
 </script>
 
@@ -12,15 +15,10 @@ const { x, y } = useMouse()
     <div class="flex flex-col text-gray-400 text-xs">{{ x }}:{{ y }}</div>
     <div class="flex flex-col custom-shortcut">Index Page</div>
     <div class="flex flex-row gap-1 text-3xl">
-      <!-- A basic anchor icon from Phosphor icons -->
       <div class="i-ph-anchor-simple-thin" />
-      <!-- An orange alarm from Material Design Icons -->
       <div class="i-mdi-alarm text-orange-400 hover:text-blue-400" />
-      <!-- A large Vue logo -->
       <div class="i-logos-vue text-5xl" />
-      <!-- Sun in light mode, Moon in dark mode, from Carbon -->
-      <div class="i-carbon-sun dark:i-carbon-moon" />
-      <!-- Twemoji of laugh, turns to tear on hovering -->
+      <div class="i-carbon-sun dark:i-carbon-moon" @click="toggleDark()" />
       <div
         class="i-twemoji-grinning-face-with-smiling-eyes hover:i-twemoji-face-with-tears-of-joy"
       />
