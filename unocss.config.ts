ff7abeed49ab286b1067b633a7f0c5e11b75ccbb
Alexander Semyenov <semyenov@hotmail.com>
2022-11-14 06:59:23 +0300

    .

--- unocss.config.ts
+++ unocss.config.ts
@@ -9,26 +9,14 @@ import {
   transformerVariantGroup,
 } from 'unocss'
 
+import { range } from '@antfu/utils'
+
 import { createShortcuts } from './uno'
 const shortcuts = createShortcuts()
 
 export default defineConfig({
   rules: [['custom-rule', { color: 'red' }]],
-  safelist: Object.keys(shortcuts).concat([
-    'gap-1',
-    'gap-1.5',
-    'gap-2',
-    'gap-2.5',
-    'gap-3',
-    'gap-3.5',
-    'gap-4',
-    'gap-4.5',
-    'gap-5',
-    'gap-5.5',
-    'gap-6',
-    'gap-6.5',
-  ]),
-
+  safelist: Object.keys(shortcuts).concat(range(12).map((i) => `gap-${i / 2}`)),
   presets: [
     presetAttributify(),
     presetIcons(),
