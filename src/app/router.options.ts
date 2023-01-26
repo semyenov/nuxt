1e1bd8a4ebfa835db17cf26884fc8189cedb47cf
Alexander Semyenov <semyenov@hotmail.com>
2023-01-26 12:37:04 +0300

    .

--- src/app/router.options.ts
+++ src/app/router.options.ts
@@ -1,7 +1,11 @@
+import qs from 'qs'
 import type { RouterOptions } from '@nuxt/schema'
 
 // https://router.vuejs.org/api/interfaces/routeroptions.html
 export default <RouterOptions>{
+  parseQuery: qs.parse,
+  stringifyQuery: qs.stringify,
+
   scrollBehavior(_from, _to, savedPosition) {
     if (savedPosition) {
       return savedPosition
